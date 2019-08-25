1162eb99d9b6428a09dd8ee2d61a55e127046622
Sungil Saul Lee <jangsoo86@gmail.com>
2019-08-25 15:44:55 -0700

    Variable reference types

--- Main.java
+++ Main.java
@@ -1,11 +1,10 @@
 import java.lang.*;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
         byte age = 30;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = true;
+        Date now = new Date();
+        System.out.println(now);
     }
 }
