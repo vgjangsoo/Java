6b2e0cb1ecb0ff0829149351a759195e56aa560d
Sungil Saul Lee <jangsoo86@gmail.com>
2019-08-25 15:38:10 -0700

    Storing variables

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
 import java.lang.*;
 
-class Main {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Wold");
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = true;
     }
 }
