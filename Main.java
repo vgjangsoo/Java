ec6a3a179388e762bb587fccf754099e9523dd53
Sungil Saul Lee <jangsoo86@gmail.com>
2019-08-26 09:06:01 -0700

    Java String class

--- Main.java
+++ Main.java
@@ -2,9 +2,11 @@ import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
-        Point point1 = new Point(1, 1);
-        Point point2 = point1;
-        point1.x = 2;
-        System.out.println(point2);
+        String message = "Hello World" + "!!";
+        System.out.println(message);
+        System.out.println(message.endsWith("!!"));
+        System.out.println(message.length());
+        System.out.println(message.indexOf("H"));
+        System.out.println(message.replace("!", "*"));
     }
 }
