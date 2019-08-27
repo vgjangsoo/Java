cf25d8c12af6491cf099ba2622a761f64dad985a
Sungil Saul Lee <jangsoo86@gmail.com>
2019-08-26 18:31:44 -0700

    Multi-dimensional arrays

--- Main.java
+++ Main.java
@@ -3,9 +3,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = { 2, 3, 5, 1, 4 };
-        Arrays.sort(numbers);
-        System.out.println(numbers.length);
-        System.out.println(Arrays.toString(numbers));
+        int[][] numbers = { { 1, 2, 3 }, { 4, 5, 6 } };
+        System.out.println(Arrays.deepToString(numbers));
     }
 }
