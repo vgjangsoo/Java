16db4a1a00a1052911d41096a124fcfc68059981
Sungil Saul Lee <jangsoo86@gmail.com>
2019-08-26 18:34:21 -0700

    Constant variables

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] numbers = { { 1, 2, 3 }, { 4, 5, 6 } };
-        System.out.println(Arrays.deepToString(numbers));
+        // all caps for the constant.
+        final float PI = 3.14F;
     }
 }
