08f4ec4b744cf36e8412aefb6a815725a1d5809d
Sungil Saul Lee <jangsoo86@gmail.com>
2019-08-27 12:57:19 -0700

    Formatting Numbers

--- Main.java
+++ Main.java
@@ -1,21 +1,12 @@
+import java.text.NumberFormat;
+
 public class Main {
     public static void main(String[] args) {
-        int result = Math.round(1.1F);
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String result = currency.format(1234567.891);
         System.out.println(result);
 
-        int result2 = (int) Math.ceil(1.1F);
+        String result2 = NumberFormat.getPercentInstance().format(0.1);
         System.out.println(result2);
-
-        int result3 = (int) Math.floor(1.1F);
-        System.out.println(result3);
-
-        int result4 = Math.max(1, 2);
-        System.out.println(result4);
-
-        double result5 = Math.random();
-        System.out.println(result5);
-
-        int result6 = (int) (Math.random() * 100);
-        System.out.println(result6);
     }
 }
